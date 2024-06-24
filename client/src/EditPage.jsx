269c573b39a2368f9ba996b53cea0d051d68d5b5
milo.masonicic <ilom3774@gmail.com>
2024-06-24 14:53:09 +0200

    addEditPublishPage

--- client/src/EditPage.jsx
+++ client/src/EditPage.jsx
@@ -1,27 +1,53 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
+import axios from 'axios'
+import { useRef } from 'react';
 
 export default function EditPage(){
 
     const location = useLocation();
     const { order } = location.state || {};
+    const descriptionRef = useRef(null)
 
     const formContainer = {
         margin: '0 auto',
-        
         background: 'linear-gradient(90deg, rgba(255,252,252,1) 0%, rgba(225,225,227,1) 35%, rgba(255,255,255,1) 100%)',
         padding:'20px'
-
     }
-
     
     const inputFile = {
         margin: '0 auto',
-        minWidth:'256px',
-        
+        minWidth:'256px',        
         padding:'10px',
         marginTop:'15px'
+    }
 
+    
+     //url - testing URL
+   
+     const url = "https://jsonplaceholder.typicode.com"
+    //formData
+    
+    const handleSubmit = async(e) => {
+        e.preventDefault();
+
+        const newItem = {
+            id: 1,
+            seller: order.seller,
+            description: descriptionRef.current,
+            price: "222",
+            name: order.productName
+          };
+      
+
+        try{
+            const response = await axios.post(`${url}/posts`, newItem)
+
+            console.log(response.data)
+        }
+        catch(error){
+            console.log('Error', error)
+        }
     }
 
     
@@ -33,33 +59,25 @@ export default function EditPage(){
       <h1>Edit and Publish Item</h1>
       {order ? (
         <div>
-           <form action="" style={formContainer}>
+           <form action="" style={formContainer} onSubmit={handleSubmit}>
                 <div>
+                    <div>Seller:</div>
                     <input style={inputFile} type="text" value={order.seller}/>
                 </div>
 
                 <div>
+                    <div>Price:</div>
                     <input style={inputFile} type="text" value={order.price}/>
                 </div>
 
                 <div>
-                    <input style={inputFile} type="text" value={order.price}/>
-                </div>
-
-                <div>
+                    <div>Name:</div>
                     <input style={inputFile} type="text" defaultValue={order.productName} />
                 </div>
 
                 <div>
-                    <div>
-                        Image:
-
-                    </div>
-                    <input type="file" />
-                </div>
-
-                <div>
-                    <textarea  style={{...inputFile, height:'90px'}} name="" id="" placeholder='Descritpion'></textarea>
+                    <div>Description:</div>
+                    <textarea  style={{...inputFile, height:'110px'}} value={descriptionRef} name="" id="" placeholder='Descritpion'></textarea>
                 </div>
                 
                 <button type='submit'>Publish</button>
@@ -73,3 +91,14 @@ export default function EditPage(){
         </div>
     )
 }
+
+/*
+
+
+                <div>
+                    <div>
+                        Image:
+
+                    </div>
+                    <input type="file" />
+                </div>*/
